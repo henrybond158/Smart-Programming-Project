6c3a8a2d0e45e38ba3607c6927f5a4a48a36a73e
Mathew Payne <geekmasher@gmail.com>
2014-02-22 20:23:05 +0000

    Adding Python Code

---
+++ car.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+
+import sys, select, tty, termios, bluetooth, time
+
+from evdev import InputDevice, categorize, ecodes
+
+
+def connect(bdr_addr, port):
+	
+	sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+	sock.connect((bdr_addr, port))
+
+	return sock	
+	
+	
+	
+def movement(dir, speed):
+
+	print "shit"
+
+
+def stop():
+	
+	print"gah"	
+	
+	
+if __name__ == '__main__':
+
+
+
+
+	dev = InputDevice('/dev/input/event0')
+	sock = connect("00:12:05:09:92:74",1)
+	
+		
+	for event in dev.read_loop():
+		if event.type == ecodes.EV_KEY:
+
+			key_pressed = str(categorize(event))
+			if 'KEY_W' in key_pressed:
+				sock.send('\x1F')
+				time.sleep(0.035)
+			if 'KEY_S' in key_pressed:
+                                sock.send('\x2F')
+                                time.sleep(0.035)
+			if 'KEY_A' in key_pressed:
+                                sock.send('\x5F')
+                                time.sleep(0.035)
+			if 'KEY_D' in key_pressed:
+                                sock.send('\x6F')
+                                time.sleep(0.035)
+			if '' in key_pressed:
+				sock.send('\x00')
+		
+
+
