bc32a7bcdf5f695dcdf050a8e8a14c91cd9bf9dc
Henry B <henrybond158@icloud.com>
2014-03-13 10:10:35 +0000

    added cli menu fix

--- init.py
+++ init.py
@@ -11,3 +11,27 @@ try:
 	mode = int(raw_input('enter your choice:'))
 except: ValueError:
 	print "not a number"
+
+if mode = 1:
+	print('calling the mac address change function')
+	macAddressChanging()
+elif mode = 2:
+	print('calling the keyboard control function')
+	#keyboard call
+elif mode = 3:
+	print('calling the gui control program')
+	#gui stuff here
+elif mode = 4
+	print('calling the gamepad control function')
+	#game pad stuff
+
+
+
+def macAddressChanging
+	try:
+		mac = raw_input('Please enter new mac address')
+		f = open('macFile','w')
+		f.write(mac)
+		f.close
+	except:
+		print('something went wrong')
