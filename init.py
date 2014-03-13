cb498126e8cedc11406a349c3abb9cdf8232984e
root <root@localhost.localdomain>
2014-03-13 10:17:27 +0000

    added changes to init

--- init.py
+++ init.py
@@ -9,25 +9,25 @@ print('4: gamepad controller')
 raw_input('Enter Your Choice')
 try:
 	mode = int(raw_input('enter your choice:'))
-except: ValueError:
+except ValueError:
 	print "not a number"
 
-if mode = 1:
+if mode == 1:
 	print('calling the mac address change function')
 	macAddressChanging()
-elif mode = 2:
+elif mode == 2:
 	print('calling the keyboard control function')
 	#keyboard call
-elif mode = 3:
+elif mode == 3:
 	print('calling the gui control program')
 	#gui stuff here
-elif mode = 4
+elif mode == 4:
 	print('calling the gamepad control function')
 	#game pad stuff
 
 
 
-def macAddressChanging
+def macAddressChanging():
 	try:
 		mac = raw_input('Please enter new mac address')
 		f = open('macFile','w')
