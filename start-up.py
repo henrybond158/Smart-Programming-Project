47c3e936bd54232ddce55ae8a26927a249a5f400
root <root@localhost.localdomain>
2014-03-27 10:45:44 +0000

    Edited start-up.py to add error checking and inprove functions

--- start-up.py
+++ start-up.py
@@ -9,12 +9,16 @@ from lib import xbox_read                				# Controller Lib
 #======> Car functions:
 
 def connect(bdr_addr, port):
-    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    sock.connect((bdr_addr, port))
-    return sock
+	try:
+	    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+	    sock.connect((bdr_addr, port))
+	    return sock
+	except:
+		return ''
 
-def movement(stuff): sock.send(stuff)
-def move(dir, spd): sock.send(chr((dir*16) + spd))
+def move(dir, spd):
+	if (dir < 15) or (spd < 15):
+		sock.send(chr((dir*16) + spd))
 
 #=====> Devices:
 
@@ -24,17 +28,18 @@ def keyboard():
     for event in dev.read_loop():
         if event.type == ecodes.EV_KEY:
             key = categorize(event)
-            if 'KEY_W' in str(key) and (key.keystate == key.key_down or key.keystate == key.key_hold): movement('\x1A')
-            if 'KEY_S' in str(key) and (key.keystate == key.key_down or key.keystate == key.key_hold): movement('\x2A')
-            if 'KEY_A' in str(key) and (key.keystate == key.key_down or key.keystate == key.key_hold): movement('\x3A')
-            if 'KEY_D' in str(key) and (key.keystate == key.key_down or key.keystate == key.key_hold): movement('\x4A')
-            if 'KEY_Q' in str(key) and (key.keystate == key.key_down or key.keystate == key.key_hold): movement('\x5A')
-            if 'KEY_E' in str(key) and (key.keystate == key.key_down or key.keystate == key.key_hold): movement('\x6A')
 
-            if key.keystate == key.key_up: movement('\x00')
+            if 'KEY_W' in str(key) and (key.keystate == key.key_down): move(1,15)
+            if 'KEY_S' in str(key) and (key.keystate == key.key_down): move(2,15)
+            if 'KEY_A' in str(key) and (key.keystate == key.key_down): move(3,15)
+            if 'KEY_D' in str(key) and (key.keystate == key.key_down): move(4,15)
+            if 'KEY_Q' in str(key) and (key.keystate == key.key_down): move(5,15)
+            if 'KEY_E' in str(key) and (key.keystate == key.key_down): move(6,15)
+
+            if (! key.keystate == key.key_hold): move(0,0)
             if 'KEY_ESC' in str(key): break
-    print '[...] Stoping Keyboard [...]'
 
+    print '[...] Stoping Keyboard [...]'
 def controllerXbox():
     try:
         for event in xbox_read.event_stream(deadzone=12000):
@@ -47,7 +52,6 @@ def controllerXbox():
             if event.key == 'X1' and event.value < 1: move(6,(int(event.value) /2200))
     except:
         print '[...] Error With Controller [...]'
-
 def controllerPs3():
 	for event in xbox_read.event_stream(deadzone=12000):
 		print event
@@ -61,10 +65,13 @@ if __name__ == '__main__':
 			print macaddr
 		# TODO: go through all theMACs if it fails to connect
 	except IOError: #should be unecesary cause "r+" should create a file if it doesn't exist
-		print "The MAC file does not seem to exist."
+		print "[...] MAC address File doesn't seem to Exist [...]"
 
     sock = connect(macaddr,1)
+    if sock == '':
+    	print '[...] Error When Connecting to Car [...]'
+	else:
+		controllerPs3()
+    	sock.close()
 
-    controllerPs3()
-    sock.close()
     print "[...] Program stopped [...]"
