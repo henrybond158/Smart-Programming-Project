c2e8e936bbfa4a1dbe4dff451b462212f8fe9af2
root <root@localhost.localdomain>
2014-03-27 11:38:13 +0000

    Adding and fixing new functional

--- PiProgram.py
+++ PiProgram.py
@@ -7,7 +7,7 @@ import sys, select, tty, termios, bluetooth, time
 from evdev import InputDevice, categorize, ecodes       # Device Input
 from lib import xbox_read                               # Controller Lib
 
-
+# =====> GUI Class
 class Base:
     def destroy(self, widget, data=None):
         print('you closed the window')
@@ -65,7 +65,15 @@ class Base:
         self.window.show_all()
         self.window.connect("destroy",self.destroy)
 
-# =====> Connection
+    def main(self):
+        gtk.main()
+
+# =====> Car Class
+class Car:
+    def __init__(self):
+        print '[...] Starting up the Car [...]'
+        x = 0
+        y = 0
 
     def connecting(self,bdr_addr):
         try:
@@ -74,20 +82,16 @@ class Base:
             return sock
         except:
             return ''
-    def move(self, dir, spd): sock.send(chr((dir*16) + spd))
 
-# =====> Devices
+    def moveX(self, st): x = st
+    def moveY(self, st): y = st
+    def move(self, spd):
+        arra = [[5,1,6], [3,0,4], [7,2,8]]
+        sock.send(chr((dir * arra[y + 1][x + 1]) + spd))
+        x = 0
+        y = 0
 
-    def controllerXbox(self):
-        try:
-            for event in xbox_read.event_stream(deadzone=12000):
-
-                if event.key == 'Y1' and event.value > 1: move(1,(int(event.value) /2200))
-                if event.key == 'Y1' and event.value < 1: move(2,(int(event.value) /2200))
-                if event.key == 'X1' and event.value > 1: move(5,(int(event.value) /2200))
-                if event.key == 'X1' and event.value < 1: move(6,(int(event.value) /2200))
-        except:
-            print '[...] Error With Controller [...]'
+    
     def keyboard(self):
         try:
             dev = InputDevice('/dev/input/event0')
@@ -96,24 +100,33 @@ class Base:
                 if event.type == ecodes.EV_KEY:
                     key = categorize(event)
 
-                    if 'KEY_W' in str(key) and (key.keystate == key.key_down): move(1,15)
-                    if 'KEY_S' in str(key) and (key.keystate == key.key_down): move(2,15)
-                    if 'KEY_A' in str(key) and (key.keystate == key.key_down): move(3,15)
-                    if 'KEY_D' in str(key) and (key.keystate == key.key_down): move(4,15)
-                    if 'KEY_Q' in str(key) and (key.keystate == key.key_down): move(5,15)
-                    if 'KEY_E' in str(key) and (key.keystate == key.key_down): move(6,15)
+                    if 'KEY_W' in str(key) and (key.keystate == key.key_down): moveY(1)
+                    if 'KEY_S' in str(key) and (key.keystate == key.key_down): moveY(-1)
+                    if 'KEY_A' in str(key) and (key.keystate == key.key_down): moveX(-1)
+                    if 'KEY_D' in str(key) and (key.keystate == key.key_down): moveX(1)
 
-                    if (! key.keystate == key.key_hold): move(0,0)
+                    if (key.keystate != key.key_hold): move(15)
                     if 'KEY_ESC' in str(key): break
                     
             print '[...] Stoping Keyboard [...]'
         except:
             print '[...] Error With Keyboard [...]'
+    def controllerXbox(self):
+        print '[...] Xbox Controller [...]'
+        try:
+            for event in xbox_read.event_stream(deadzone=12000):
 
-    def main(self):
-        gtk.main()
+                if event.key == 'Y1' and event.value > 1: moveY(1)
+                if event.key == 'Y1' and event.value < 1: moveY(-1)
+                if event.key == 'X1' and event.value > 1: moveX(1)
+                if event.key == 'X1' and event.value < 1: moveX(-1)
 
-  
+                move((int(event.value) /2200))
+        except:
+            print '[...] Error With Controller [...]'
+    def controllerPs3(self):
+        print '[...] Ps3 Controller [...]'
+
 if __name__ == "__main__":
     base = Base()
     try:
@@ -122,7 +135,7 @@ if __name__ == "__main__":
             print macaddr
         # TODO: go through all theMACs if it fails to connect
     except IOError: #should be unecesary cause "r+" should create a file if it doesn't exist
-        print "The MAC file does not seem to exist."
+        print "[...] MAC address File doesn't seem to Exist [...]"
 
     # sock = base.connecting("00:12:05:09:97:76")
     sock = base.connecting(macaddr)
