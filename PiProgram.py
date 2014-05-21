276cea3498085dc00cc0504a7ce9e620b6993be5
root <geekmasher@gmail.com>
2014-05-21 12:21:23 +0100

    fixed parts added pre difenid functions

--- PiProgram.py
+++ PiProgram.py
@@ -14,192 +14,242 @@ from evdev import InputDevice, categorize, ecodes # Device Input
 
 class Base(gtk.Window):
 
-    def destroy(self, widget, data=None):
-        print('you closed the window')
-        gtk.main_quit()
-    def forward(self, widget, data=None):
-        print('you click the forward button')
-    def backwards(self, widget, data=None):
-        print('you clicked the back button')
-    def left(self, widget, data=None):
-        print('you clicked the left button')
-    def right(self, widget, data=None):
-        print('you clicked the right button')
-    def cruise(self, widget, data=None):
-        print("you be crusing")
-    def selection_changed(self, widget, data=None):
-        print ("keyboard selected")
-    def xboxController(self, widget, data=None):
-        print ("xbox selected")
-    def wheelController(self, widget, data=None):
-        print("wheel selected")
-    def selection_changed( self, w, data=None):
-        self.label.set_label( "Current selection: <b>%s</b>" % data)
+	def destroy(self, widget, data=None):
+		print('you closed the window')
+		gtk.main_quit()
+	def forward(self, widget, data=None):
+		print('you click the forward button')
+	def backwards(self, widget, data=None):
+		print('you clicked the back button')
+	def left(self, widget, data=None):
+		print('you clicked the left button')
+	def right(self, widget, data=None):
+		print('you clicked the right button')
+	def cruise(self, widget, data=None):
+		print("you be crusing")
+	def selection_changed(self, widget, data=None):
+		print ("keyboard selected")
+	def xboxController(self, widget, data=None):
+		print ("xbox selected")
+	def wheelController(self, widget, data=None):
+		print("wheel selected")
+	def selection_changed( self, w, data=None):
+		self.label.set_label( "Current selection: <b>%s</b>" % data)
 
-    def __init__(self):
-        os.system(['clear','cls'][os.name == 'nt'])
-        menu = {}
+	def __init__(self):
+		os.system(['clear','cls'][os.name == 'nt'])
+		menu = {}
 
-        print "[...] Connecting to the Car [...]"
+		print "[...] Connecting to the Car [...]"
 
-        if Car().test("00:12:05:09:94:45"):
-            print "[...]\033[92m Connection Successful \033[0m[...]"
-        else:
-            print "[...]\033[91m Connection Failed \033[0m [...]"
+		if Car().test("00:12:05:09:94:45"):
+			print "[...]\033[92m Connection Successful \033[0m[...]"
+		else:
+			print "[...]\033[91m Connection Failed \033[0m [...]"
 
 
-        carClass = Car()
-        carClass.connecting("00:12:05:09:94:45")
+		carClass = Car()
+		carClass.connecting("00:12:05:09:94:45")
 
-        menu['1']=": Keyboard"
-        menu['2']=": Wheel"
-        menu['3']=": Xbox Controller"
-        menu['4']=": Playstation3 Controller"
-        menu['m']=": Change the Mac Address"
-        menu['q']=": Quit"
+		menu['1']=": Keyboard"
+		menu['2']=": Wheel"
+		menu['3']=": Xbox Controller"
+		menu['4']=": Playstation3 Controller"
+		menu['5']=": Pre-set Figures"
+		menu['m']=": Change the Mac Address"
+		menu['q']=": Quit"
 
-        while True: 
-            print "[...] Menu [...]\n"
-            options=menu.keys()
-            options.sort()
+		while True: 
+			print "[...] Menu [...]\n"
+			options=menu.keys()
+			options.sort()
 
-            for entry in options: 
-                print "\t" + entry, menu[entry]
+			for entry in options: 
+				print "\t" + entry, menu[entry]
 
-            selection=raw_input("\nPlease Select: ") 
-            os.system(['clear','cls'][os.name == 'nt'])
+			selection=raw_input("\nPlease Select: ") 
+			os.system(['clear','cls'][os.name == 'nt'])
 
-            if selection == '1': 
-                print '[...] Keyboard [...]'
-                carClass.keyboard()
-            elif selection == '2': 
-                print '[...] Wheel [...]'
-                carClass.wheelHandler()
-            elif selection == '3': 
-                print '[...] Xbox Controller [...]'
-                carClass.controllerXbox()
-            elif selection == '4': 
-                print '[...] Playstation3 Controller [...]'
-                carClass.controllerPs3()
-            elif selection == 'm':
-                self.getMacAddress(True)
-            elif selection == 'q':
-                break
-            else: 
-                print "Unknown Option Selected!"
-                time.sleep(1)
+			if selection == '1': 
+				print '[...] Keyboard [...]'
+				carClass.keyboard()
+			elif selection == '2': 
+				print '[...] Wheel [...]'
+				carClass.wheelHandler()
+			elif selection == '3': 
+				print '[...] Xbox Controller [...]'
+				carClass.controllerXbox()
+			elif selection == '4': 
+				print '[...] Playstation3 Controller [...]'
+				carClass.controllerPs3()
+			elif selection == '5':
+				carClass.subPreMenu()
+			elif selection == 'm':
+				self.getMacAddress(True)
+			elif selection == 'q':
+				break
+			else: 
+				print "Unknown Option Selected!"
+				time.sleep(1)
+	
 
 # =====> Car Class
 class Car:
-    x = 1
-    y = 1
-    last = 0
-    wheelClass = wheel.WheelClass()
+	x = 1
+	y = 1
+	last = 0
+	wheelClass = wheel.WheelClass()
 
-    #def __init__(self):
+	#def __init__(self):
 
 
-    def moveX(self, st): self.x = st
-    def moveY(self, st): self.y = st
-    def moveXY(self, xBit, yBit, spd, time):
-    	self.x = xBit
-    	self.y = yBit
-    	self.move(spd)
-    	time.sleep(time)
+	def moveX(self, st): self.x = st
+	def moveY(self, st): self.y = st
+	def moveXY(self, xBit, yBit, spd, tim):
+		self.x = xBit
+		self.y = yBit
+		self.move(spd)
+		time.sleep(tim)
 
-    def move(self, spd):
-        arra = [[5,1,6], [3,0,4], [7,2,8]]
-        ch = (16 * arra[self.y][self.x]) + spd
-        if self.last != ch:
-            self.sock.send(chr(ch))
-            self.last = ch
+	def move(self, spd):
+		arra = [[5,1,6], [3,0,4], [7,2,8]]
+		ch = (16 * arra[self.y][self.x]) + spd
+		if self.last != ch:
+			self.sock.send(chr(ch))
+			self.last = ch
 
-    def keyboard(self):
-        # loop around each key press
-        while True:
-        # Starts pulling keyboard inputs from pygame
-            pygame.event.pump()
-        # Sets keyboard inputs to a variable
-            self.pressed = pygame.key.get_pressed()
+	def keyboard(self):
+		# loop around each key press
+		while True:
+		# Starts pulling keyboard inputs from pygame
+			pygame.event.pump()
+		# Sets keyboard inputs to a variable
+			self.pressed = pygame.key.get_pressed()
 
-        # if either the up/down button is pressed, set the Y axes to
-            if self.pressed[K_UP]: self.moveY(0)
-            elif self.pressed[K_DOWN]: self.moveY(2)
-            else: self.moveY(1)
+		# if either the up/down button is pressed, set the Y axes to
+			if self.pressed[K_UP]: self.moveY(0)
+			elif self.pressed[K_DOWN]: self.moveY(2)
+			else: self.moveY(1)
 
-        # if either the left/right button is pressed, set the X axes to
-            if self.pressed[K_LEFT]: self.moveX(0)
-            elif self.pressed[K_RIGHT]: self.moveX(2)
-            else: self.moveX(1)
+		# if either the left/right button is pressed, set the X axes to
+			if self.pressed[K_LEFT]: self.moveX(0)
+			elif self.pressed[K_RIGHT]: self.moveX(2)
+			else: self.moveX(1)
 
-        # Will run the move function, move with set the X/Y vars and move the car accordingly
-            self.move(8)
-        # If the escape key is pressed, exit
-            if self.pressed[K_ESCAPE]: break
-    def controllerXbox(self):
-        # Will catch errors
-        try:
-        # loop around xbox events
-            for event in xbox_read.event_stream(deadzone=12000):
+		# Will run the move function, move with set the X/Y vars and move the car accordingly
+			self.move(8)
+		# If the escape key is pressed, exit
+			if self.pressed[K_ESCAPE]: break
+	def controllerXbox(self):
+		# Will catch errors
+		try:
+		# loop around xbox events
+			for event in xbox_read.event_stream(deadzone=12000):
 
-            # if either the up/down button is pressed, set the Y axes to
-                if event.key == 'Y1' and event.value > 1: self.moveY(2)
-                elif event.key == 'Y1' and event.value < 1: self.moveY(0)
-                else: self.moveY(1)
+			# if either the up/down button is pressed, set the Y axes to
+				if event.key == 'Y1' and event.value > 1: self.moveY(2)
+				elif event.key == 'Y1' and event.value < 1: self.moveY(0)
+				else: self.moveY(1)
 
-            # if either the left/right button is pressed, set the X axes to
-                if event.key == 'X1' and event.value > 1: self.moveX(2)
-                elif event.key == 'X1' and event.value < 1: self.moveX(0)
-                else: self.moveX(1)
+			# if either the left/right button is pressed, set the X axes to
+				if event.key == 'X1' and event.value > 1: self.moveX(2)
+				elif event.key == 'X1' and event.value < 1: self.moveX(0)
+				else: self.moveX(1)
 
-                move((int(event.value) /2200))
-        except:
-            print '[...] Error With Controller [...]'
-    def controllerPs3(self):
-        print '[...] Ps3 Controller [...]'
-    def wheelHandler(self):
-        while True:
-            direction,turning,speed=self.wheelClass.getMov()
-            self.y = direction
-            self.x = turning
-            self.move(speed)
+				move((int(event.value) /2200))
+		except:
+			print '[...] Error With Controller [...]'
+	def controllerPs3(self):
+		print '[...] Ps3 Controller [...]'
+	def wheelHandler(self):
+		while True:
+			direction,turning,speed=self.wheelClass.getMov()
+			self.y = direction
+			self.x = turning
+			self.move(speed)
 
-    def connecting(self,bdr_addr):
-        try:
-            self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-            self.sock.connect((bdr_addr, 1))
-            return sock
-        except: return ''
-    def test(self,mac):
-        try:
-            testSock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-            testSock.connect((mac, 1))
+	def connecting(self,bdr_addr):
+		try:
+			self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+			self.sock.connect((bdr_addr, 1))
+			return sock
+		except: return ''
+	def test(self,mac):
+		try:
+			testSock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+			testSock.connect((mac, 1))
 
-            testSock.close()
-            time.sleep(.5)
-            return True
-        except:
-            return False
+			testSock.close()
+			time.sleep(.5)
+			return True
+		except:
+			return False
 
-    ##### Accelerometer #####
-    def axelmeter(self, speed):
-        self.progbar.set_fraction(speed/11.0)
-class PreSetFunctions:
+	def subPreMenu(self):
+		os.system(['clear','cls'][os.name == 'nt'])
+		menu = {}
+
+		menu['1']=": Right Circle"
+		menu['2']=": Left Circle"
+		menu['3']=": Three Point Turn"
+		menu['4']=": Figure of Eigth"
+		menu['q']=": Quit"
+
+		while True: 
+			print "[...] Pre-defined Figures [...]\n"
+			options=menu.keys()
+			options.sort()
+
+			for entry in options: 
+				print "\t" + entry, menu[entry]
+
+			selection=raw_input("\nPlease Select: ") 
+			os.system(['clear','cls'][os.name == 'nt'])
+
+			if selection == '1': 
+				print '[...] Right Circle [...]'
+				self.rightCircle()
+			elif selection == '2': 
+				print '[...] Left Circle [...]'
+				self.leftCircle()
+			elif selection == '3': 
+				print '[...] Three Point Turn [...]'
+				self.threePointTurn()
+			elif selection == '4': 
+				print '[...] Figure of Eigth [...]'
+				self.eigth()
+			elif selection == 'q':
+				break
+			else: 
+				print "Unknown Option Selected!"
+				time.sleep(1)
+
+
 	def threePointTurn(self):
-		moveXY(1, 2, 10, 1)		# backwards
-		moveXY(2, 2, 10, 1.5)	# backwards, right
-		moveXY(0, 0, 10, 1.5)	# forward, left
-		moveXY(1, 0, 10, 1)		# forward
+		self.moveXY(1, 2, 8, .4)		# backwards
+		self.moveXY(2, 2, 8, 1.6)			# backwards, right
+		self.moveXY(0, 0, 8, 1.6)			# forward, left
+		self.moveXY(1, 0, 8, .8)		# forward
+		self.moveXY(1,1,0,0)
 
 	def rightCircle(self):
-		moveXY(2, 0, 10, 10)	# forward right
+		self.moveXY(2, 0, 10, 10)	# forward right
+		self.moveXY(1,1,0,0)		# stop
+
 	def leftCircle(self):
-		moveXY(0, 0, 10, 10)	# forward left
+		self.moveXY(0, 0, 10, 10)	# forward left
+		self.moveXY(1,1,0,0)		# stop
+
 	def eigth(self):
-		moveXY(2, 0, 10, 4)		# forward right
-		moveXY(1, 0, 10, .2)	# forward
-		moveXY(0, 0, 10, 10)	# forward left
-		moveXY(1, 0, 10, .2)	# forward
+		self.moveXY(2, 0, 10, 4)		# forward right
+		self.moveXY(1, 0, 10, .2)	# forward
+		self.moveXY(0, 0, 10, 10)	# forward left
+		self.moveXY(1, 0, 10, .2)	# forward
+		self.moveXY(1,1,0,0)		# stop
+
+	##### Accelerometer #####
+	def axelmeter(self, speed):
+		self.progbar.set_fraction(speed/11.0)
 
 if __name__ == "__main__":
-    Base()
+	Base()
