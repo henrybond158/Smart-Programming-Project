6b6e6d0f59e8ce918a2f2d8d3b889bb6cd3da24e
root <root@raspberrypi.(none)>
2014-05-22 11:46:09 +0000

    made wheel work

--- wheel.py
+++ wheel.py
@@ -1,5 +1,5 @@
-#/usr/bin/python
-import os
+#/usr/bin/python2
+
 import pygame
 import sys
 import time
@@ -7,10 +7,8 @@ import time
 
 class WheelClass:
 	def __init__(self):
-
-
 		self.d = 0
-
+		
 		size = [600,400]
 
 		screen = pygame.display.set_mode(size)		
@@ -20,45 +18,33 @@ class WheelClass:
 
 		self.joystick = pygame.joystick.Joystick(0)
 		self.joystick.init()
-		
-
 	def getMov(self):
+	
 		pygame.event.pump()
-		#sys.stdout.write("WHEEL: ")
-
-                #print self.joystick.get_axis(0)
+	        #print self._joystick.get_axis(0) #WHEEL
 		if (self.joystick.get_axis(0) > 0.075):
-			turning = 2
-			#print "turning right"
+			turning=2 #right
 		elif (self.joystick.get_axis(0) < -0.055):
-			turning = 0
-			#print "turning left"
+			turning=0 #left
 		else:
-			turning = 1
-			#print "straight line"
-		#sys.stdout.write("CLUTCH: ")
-		#print self.joystick.get_axis(1) 	
-		#sys.stdout.write("THROTTLE: ")
-		speed = (self.joystick.get_axis(1) * -1)
-		#speed = 1 - speed
+			turning=1 #straight
+
+		#print self._joystick.get_axis(1) #THROTTLE/BRAKE
+		speed = (self.joystick.get_axis(1) * -1) #THROTTLE is negative
 		speed = int(speed * 15)
+		print speed
 		if (speed > 0):
-			direction = 2
-		else:
 			direction = 0
-		#print self.joystick.get_axis(2) 
-		#rSpeed = 0.0 #(self.joystick.get_axis(3) + 1) / 2
-		#rSpeed = 1 - rSpeed
-		#rSpeed = int(rSpeed * 15)
-		if (direction == 0):
-			speed = (self.joystick.get_axis(1))
-			speed = int(speed * 15.1)
-			if (speed > 0):
-				direction = 1
-		#print speed
-		print direction
-		#sys.stdout.write("BRAKE: ")
-		#print self.joystick.get_axis(3) 
-		#print self.joystick.get_axis(4) 
-		#print self.joystick.get_axis(5) 
+		else:
+			direction = 2
+
+		if (direction == 2):
+			rSpeed = (self.joystick.get_axis(1)) #BRAKE is positive
+			rSpeed = int(rSpeed * 15) #Otherwise it will not get up to 15
+			if (rSpeed > 0):
+				#if (speed > 255): speed = 255
+				direction = 2
+				speed =  rSpeed
+				print ("this is your speed " + str(speed))
+
 		return direction, turning, speed
