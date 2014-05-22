7811fcf7b2874765470e2fe5d546c6042e0c606d
THESHade <josipjs@gmail.com>
2014-05-22 12:50:32 +0100

    wheel fixed

--- wheel.py
+++ wheel.py
@@ -7,16 +7,10 @@ import time
 
 class WheelClass:
 	def __init__(self):
-<<<<<<< HEAD
-		self.d = 0
-		
-		size = [600,400]
-=======
 
 		self.d = 0
 
 		self.size = [300,200]
->>>>>>> ed222dd65f3997bd48bda748fa58f152ff07e080
 
 		screen = pygame.display.set_mode(self.size)	
 
@@ -41,7 +35,7 @@ class WheelClass:
 		print speed
 		if (speed > 0):
 			direction = 0
-<<<<<<< HEAD
+
 		else:
 			direction = 2
 
@@ -55,27 +49,9 @@ class WheelClass:
 				print ("this is your speed " + str(speed))
 
 		return direction, turning, speed
-=======
-		#print self.joystick.get_axis(2) 
-		#rSpeed = 0.0 #(self.joystick.get_axis(3) + 1) / 2
-		#rSpeed = 1 - rSpeed
-		#rSpeed = int(rSpeed * 15)
-		if (direction == 0):
-			speed = (self.joystick.get_axis(1))
-			speed = int(speed * 15.1)
-			if (speed > 0):
-				direction = 1
-		#print speed
-		print direction
-		#sys.stdout.write("BRAKE: ")
-		#print self.joystick.get_axis(3) 
-		#print self.joystick.get_axis(4) 
-		#print self.joystick.get_axis(5) 
-		return direction, turning, speed
 
 	def launchGUI(self):
 		screen = pygame.display.set_mode(self.size)		
 		img = pygame.image.load("GUIPic.png").convert()
 		screen.blit(img, (0, 0)) #Display image at 0, 0
 		pygame.display.flip()   #Update screen
->>>>>>> ed222dd65f3997bd48bda748fa58f152ff07e080
